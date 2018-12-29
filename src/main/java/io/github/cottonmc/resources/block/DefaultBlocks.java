73270d9cc4fa431d4f2fab2053af8a6e7460be5e
Falkreon <falkreon@gmail.com>
2018-12-29 14:00:21 -0600

    Suggest builtins

--- src/main/java/io/github/cottonmc/resources/block/DefaultBlocks.java
+++ src/main/java/io/github/cottonmc/resources/block/DefaultBlocks.java
@@ -1,22 +1,68 @@
 package io.github.cottonmc.resources.block;
 
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.google.common.collect.ImmutableMap;
+
 import io.github.cottonmc.cotton.registry.CommonBlocks;
-import net.minecraft.block.*;
-import net.minecraft.block.Block.*;
+import net.fabricmc.fabric.block.FabricBlockSettings;
+import net.fabricmc.fabric.tags.FabricItemTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 
 public class DefaultBlocks {
+    /** A generic metal ore supplier, similar to the properties of coal or iron ore. */
+    private static final Supplier<Block> STONE_TIER_ORE = ()->new OreBlock(FabricBlockSettings.of(Material.STONE)
+            .hardness(3.0f)
+            .resistance(3.0f)
+            .build());
+    /** A resilient metal ore provider, similar to the properties of redstone ore. */
+    private static final Supplier<Block> IRON_TIER_ORE = ()->new OreBlock(FabricBlockSettings.of(Material.STONE)
+            .hardness(3.0f)
+            .resistance(3.0f)
+            .breakByTool(FabricItemTags.PICKAXES, 2)
+            .build());
+    /** A generic metal block supplier, based on the properties from iron_block */
+    private static final Supplier<Block> METAL_BLOCK = ()->new Block(FabricBlockSettings.of(Material.METAL)
+            .hardness(5.0f)
+            .resistance(6.0f)
+            .build());
+
+    private static final Map<String, Supplier<Block>> BUILTINS = ImmutableMap.<String, Supplier<Block>>builder()
+            .put("copper_ore", STONE_TIER_ORE) //Chalcopyrite
+            .put("silver_ore", IRON_TIER_ORE)  //Galena
+            .put("lead_ore",   IRON_TIER_ORE)  //Galena
+            .put("zinc_ore",   STONE_TIER_ORE) //Sphalerite, which is more often found in small amounts mixed with copper, lead, and iron sulfides
+            .put("uranium_ore", IRON_TIER_ORE) //Pitchblende
+            
+            .put("copper_block",    METAL_BLOCK)
+            .put("silver_block",    METAL_BLOCK)
+            .put("lead_block",      METAL_BLOCK)
+            .put("zinc_block",      METAL_BLOCK)
+            .put("steel_block",     METAL_BLOCK)
+            .put("electrum_block",  METAL_BLOCK)
+            .put("brass_block",     METAL_BLOCK)
+            .put("coal_coke_block", METAL_BLOCK)
+            .build();
 
     /**Registers an Item based on its name if it is supported or returns it if it already exists.
     * @param name The name of the block that shall be registered.
      * @return The Block associated with the given name or null if no Block for that name was found
      */
     public static Block provide(String name){
-        switch(name){
-            case "copper_ore":
-                return CommonBlocks.register(name, new OreBlock(Settings.of(Material.STONE).strength(3.0F, 3.0F)));
-            //...
-            default:
-                return null;
+        /*//We should add a get to cotton, then we can duck out of this method really early.
+        Block existing = CommonBlocks.get(name);
+        if (existing!=null) return existing;*/
+
+        Supplier<Block> supplier = BUILTINS.get(name);
+        if (supplier!=null) {
+            Block toRegister = supplier.get();
+            Block registered = CommonBlocks.register(name, toRegister);
+            return registered;
+        } else {
+            return null;
         }
     }
 }
--- src/main/java/io/github/cottonmc/resources/item/DefaultItems.java
+++ src/main/java/io/github/cottonmc/resources/item/DefaultItems.java
@@ -1,28 +1,55 @@
 package io.github.cottonmc.resources.item;
 
+import com.google.common.collect.ImmutableSet;
+
 import io.github.cottonmc.cotton.Cotton;
-import io.github.cottonmc.cotton.registry.CommonBlocks;
 import io.github.cottonmc.cotton.registry.CommonItems;
-import net.minecraft.block.Block;
-import net.minecraft.block.OreBlock;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 
 public class DefaultItems {
+    /* This list exists primarily so we can gatekeep items that we have builtin resources for, versus items we don't */
+    private static final ImmutableSet<String> BUILTINS = ImmutableSet.of(
+            /*** Pure Metals ***/
+            //Copper's a great low-tier conductor for both heat and electricity. Its high ductility is the reason we think of wires as flexible. Corrodes easily.
+            "copper_ingot", "copper_nugget", "copper_dust", "copper_gear", "copper_plate",
+            //Silver is corrosion resistant, and is a better conductor - both heat and electricity - than copper and gold. Rare, and loaded with magical lore.
+            "silver_ingot", "silver_nugget", "silver_dust", "silver_gear", "silver_plate",
+            //Lead is synonymous with heavy and slow. Used for irrigation pipes in the ancient world, also used to sink fishing bait, to plumb quality architecture, and for drywall or concrete anchors.
+            "lead_ingot",   "lead_nugget",   "lead_dust",   "lead_gear",   "lead_plate",
+            //Zinc is a secondary product of many ores, and is a key ingredient in brass. Zinc is insanely easy to corrode, to the point where they attach zinc blocks to oil rigs to keep them from rusting.
+            "zinc_ingot",   "zinc_nugget",   "zinc_dust",
+            
+            /*** Alloys and Steels ***/
+            //Steel has excellent tensile strength, good for mid- to high-tier tools, structures, and machines
+            //Typically I see around 1x iron + 4x coal dust -> 1x steel, but this should probably be a blast furnace thing.
+            "steel_ingot",  "steel_nugget",  "steel_dust",  "steel_gear",  "steel_plate",
+            //Electrum is a soft metal, not appropriate for heavy machinery, but is a corrosion-resistant conductor. It was also used to top obelisks and pyramids in ancient Egypt.
+            //Reccommended 1x gold + 1x silver -> 2x electrum
+            "electrum_ingot","electrum_nugget","electrum_dust",
+            //Brass has excellent corrosion resistance and low friction, making it great for clockwork and pneumatics, instruments, and naval applications
+            //Reccommended 3x copper + 1x zinc -> 4x brass
+            "brass_ingot",  "brass_nugget",  "brass_dust",  "brass_gear",  "brass_plate",
+            
+            /*** Misc ***/
+            "coal_dust", //needed for graphite and possibly steel
+            "coal_coke", //good second-tier power gen option
+            "mercury",   //great for magical applications, and in the ancient world was incorrectly thought to keep one healthy when drank - see also the Pythagorean Cup
+            "uranium_dust", "plutonium_dust", "thorium_dust" //don't breathe this - uranium and its byproducts can make a really great high-risk/high-reward/high-tier energy system
+            );
 
-    /**Registers an Item based on its name if it is supported or returns it if it already exists.
-     * @param name The name of the block that shall be registered if the item hasn't already been registered.
+    /**Registers an Item based on its name if it is supported, or returns it if it already exists.
+     * @param name The name of the item
      * @return The Item associated with the given name or null if the name is not supported.
      */
     public static Item provide(String name){
-        switch(name){
-            case "copper_ingot":
-                return CommonItems.register(name, new Item((new Item.Settings()).itemGroup(Cotton.commonGroup)));
-            case "silver_ingot":
-                return CommonItems.register(name, new Item((new Item.Settings()).itemGroup(Cotton.commonGroup)));
-            //...
-            default:
-                return null; //TODO: If we add a getItem method to CommonItems we could call it here.
+        /*//Having this function would let us duck out early and provide the item even if another mod provided it.
+        Item item = CommonItems.get(name);
+        if (item!=null) return item; */
+
+        if (BUILTINS.contains(name)) {
+            return CommonItems.register(name, new Item((new Item.Settings()).itemGroup(Cotton.commonGroup)));
+        } else {
+            return null;
         }
     }
 }
