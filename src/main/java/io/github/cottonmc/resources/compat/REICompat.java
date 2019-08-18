3d872bbee73d3292269fc246d819d31d3dcd8075
Danielshe <shekwancheung0528@gmail.com>
2019-08-18 23:22:00 +0800

    Update to REI 3.0

--- src/main/java/io/github/cottonmc/resources/compat/REICompat.java
+++ src/main/java/io/github/cottonmc/resources/compat/REICompat.java
@@ -1,21 +1,23 @@
 package io.github.cottonmc.resources.compat;
 
-import java.util.List;
-import java.util.Set;
-
 import com.google.common.collect.ImmutableSet;
-
 import io.github.cottonmc.resources.CottonResources;
 import io.github.cottonmc.resources.oregen.OregenResourceListener;
 import io.github.cottonmc.resources.type.ResourceType;
-import me.shedaniel.rei.api.ItemRegistry;
-import me.shedaniel.rei.api.REIPluginEntry;
+import me.shedaniel.rei.api.Entry;
+import me.shedaniel.rei.api.EntryRegistry;
+import me.shedaniel.rei.api.plugins.REIPluginV0;
+import net.fabricmc.loader.api.SemanticVersion;
+import net.fabricmc.loader.util.version.VersionParsingException;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
-public class REICompat implements REIPluginEntry {
+import java.util.List;
+import java.util.Set;
+
+public class REICompat implements REIPluginV0 {
 	public static final Identifier ID = new Identifier(CottonResources.MODID, "disable_items");
 	
 	private static final Set<String> IMMUNE_TO_HIDING = ImmutableSet.<String>of(
@@ -28,12 +30,12 @@ public class REICompat implements REIPluginEntry {
 	}
 	
 	@Override
-	public void registerItems(ItemRegistry itemRegistry) {
+	public void registerEntries(EntryRegistry entryRegistry) {
 		CottonResources.LOGGER.info("Configuring resource visibility");
-		recheckItemHiding(itemRegistry.getModifiableItemList());
+		recheckItemHiding(entryRegistry.getModifiableEntryList());
 	}
 	
-	public void recheckItemHiding(List<ItemStack> list) {
+	public void recheckItemHiding(List<Entry> list) {
 		for (ResourceType rsrc : CottonResources.BUILTINS.values()) {
 			if (IMMUNE_TO_HIDING.contains(rsrc.getBaseResource())) continue;
 			
@@ -48,25 +50,33 @@ public class REICompat implements REIPluginEntry {
 				
 				boolean add = enabled;
 				for(int i=0; i<list.size(); i++) {
-					ItemStack listItem = list.get(i);
-					if (listItem.getItem()==item) {
-						add = false;
-						if (!enabled) {
-							//System.out.println("Removing "+item.getTranslationKey());
-							list.remove(i);
-							break;
-						} else {
-							//System.out.println("Letting stay "+item.getTranslationKey());
-							break;
+					Entry entry = list.get(i);
+					if (entry.getEntryType() == Entry.Type.ITEM) {
+						ItemStack listItem = entry.getItemStack();
+						if (listItem.getItem() == item) {
+							add = false;
+							if (!enabled) {
+								//System.out.println("Removing "+item.getTranslationKey());
+								list.remove(i);
+								break;
+							} else {
+								//System.out.println("Letting stay "+item.getTranslationKey());
+								break;
+							}
 						}
 					}
 				}
 				
 				if (add) {
 					//System.out.println("Re-adding "+item.getTranslationKey());
-					list.add(new ItemStack(item));
+					list.add(Entry.create(new ItemStack(item)));
 				}
 			}
 		}
+	}
+	
+	@Override
+	public SemanticVersion getMinimumVersion() throws VersionParsingException {
+		return SemanticVersion.parse("3.0-pre");
 	}
 }
