46c10b734a3fcd431078f6dd62eea72439643af4
Jade the Cat <8322256+Jade-TheCat@users.noreply.github.com>
2020-12-09 03:44:12 -0600

    Fix copyright

--- src/main/java/io/github/cottonmc/resources/api/ResourceRequest.java
+++ src/main/java/io/github/cottonmc/resources/api/ResourceRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Jade Krabbe
+ * Copyright 2020 The Cotton Project
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
